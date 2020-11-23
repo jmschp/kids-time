01eed9ef8db32a31cefd588eef545b091c3fc9af
Miguel Pimenta <jmiguelpimenta@gmail.com>
2020-11-23 16:07:10 -0300

    added constant for ativities categories

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,3 +1,4 @@
+ACTIVITY_CATEGORIES = ["Aniversário", "Ar livre", "Casa", "Com Amigos", "Cozinha", "Livros", "Online", "Outros"]
 class Activity < ApplicationRecord
   belongs_to :user
   has_many :orders
